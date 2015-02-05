7d1ea9459e4bb33ed7f812d03f471cd0b5b658bb
Ryan Bishop <dreamchimney@gmail.com>
2015-02-04 23:16:42 -0800

    Initial commit, version 1.0

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,75 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var url = "http://dreamchimney.com/api/tracks/most_recent";
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      var artist = json[0].artist;
+      console.log("artist is " + artist);
+
+      // Conditions
+      var title = json[0].title;      
+      console.log("title are " + title);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_TEMPERATURE": artist,
+        "KEY_CONDITIONS": title
+      };
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Weather info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending weather info to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getWeather();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+  }                     
+);
